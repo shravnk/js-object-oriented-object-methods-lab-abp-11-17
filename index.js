e8ce0e3dda2c1f97baa557543694c10b31d9d6da
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-12 20:08:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+
+  this.veto = function() {
+    console.log("No, I must diagree");
+  }
+  this.approve = function() {
+    console.log("You can do that!");
+  }
+  this.doCharity = function() {
+    console.log("I like to help people.");
+  }
+  this.releasePressStatement  = function() {
+    console.log("You will see great things from Scuber");
+  }
+  this.sayHi = function() {
+    console.log(`Hi, my name is ${this.name}. I am from ${homeState}, and I was trained in ${training}`);
+  }
+}
